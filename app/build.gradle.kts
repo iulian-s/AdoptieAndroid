7c5faf5ecfa313bb756c90b261aa876eebb39716
iulian-s <04.iulian27@gmail.com>
2026-02-09 00:10:10 +0200

    Automatic release bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,15 +40,7 @@ android {
         )
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
